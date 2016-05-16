98871a471377fb08319bbb9bd76eedfe29041e93
sneffadi <sneffadi@gmail.com>
2016-05-16 11:25:53 -0600

    updates

--- index.php
+++ index.php
@@ -5,55 +5,36 @@
       <div class="small-24 columns">
       <h1>Stephanie Jelalian Portfolio</h1>
       <h3><a href="mailto:stephanie.jelalian@gmail.com">Stephanie.Jelalian@gmail.com</a>
-        <h2>Custom WordPress Foundation Themes</h2>
-          <ul>
-            <li>Created theme shell</li>
-            <li>Created ranking pages though PHP loops and generated custom fields from individual product pages</li>
-            <li>Created longform and shortform product page types</li>
-            <li>Created site-wide accessable screen options</li>
-            <li>Used both Foundation 5 &amp; 6</li>
-            <li>Created as Git Repositories</li>
-            <li>Created references plugin using JavaScript and PHP</li>
-          </ul>
-          <ul class="accordion" data-accordion data-allow-all-closed="true">
-            <li class="accordion-item" data-accordion-item data-accordion >
-              <a href="#" class="accordion-title">Site Examples</a>
-              <div class="accordion-content" data-tab-content>
-                <ul>
-                  <li><a href="http://topmaleenhancements.com/" target="_blank">TopMaleEnhancements.com</a></li>
-                  <li><a href="http://www.dietpilluniverse.com/" target="_blank">DietPillUniverse.com</a></li>
-                  <li><a href="http://naturalmaleenhancement.org/" target="_blank">NaturalMaleEnhancement.org</a></li>
-                  <li><a href="http://consumerwrinklereviews.com/" target="_blank">ConsumerWrinkleReviews.com</a></li>
-                </ul>
-              </div>
-            </li>
-            <!-- ... -->
-          </ul>
-      </div> <!-- / .small-24 columns -->
-    </div>
-    <div class="row">
+      <div class="row">
       <div class="small-24 columns">
-        <h2>Non-WordPress Foundation Sites</h2>
-          <ul class="accordion" data-accordion data-allow-all-closed="true">
-            <li class="accordion-item" data-accordion-item data-accordion >
-              <a href="#" class="accordion-title">Site Examples</a>
-              <div class="accordion-content" data-tab-content>
-                <ul>
-                  <li><a href="http://xpisupplements.com/booty-academy/" target="_blank">Booty Academy Landing Page</a></li>
-                  <li><a href="http://colovexus.com/" target="_blank">Colovexus.com</a></li>
-                  <li><a href="http://veralyze.com/" target="_blank">Veralyze.com</a></li>
-                </ul>
-              </div>
-            </li>
-            <!-- ... -->
+        <h2>Foundation Sites</h2>
+        <p>These sites I built from scratch or from a template that I had created earlier just using a Photoshop document as my design guide.</p>
+          <ul>
+            <li><a href="http://xpisupplements.com/booty-academy/" target="_blank">Booty Academy Landing Page</a></li>
+            <li><a href="http://veralyze.com/" target="_blank">Veralyze.com</a></li>
+          </ul>
+
+      </div> <!-- / .small-24 columns -->
+    </div>
+
+
+        <h2>Custom WordPress Foundation Themes</h2>
+          These are sites using WordPress themes that I built from scratch using Foundation. For these I also have built different plugins and loops in order to make the sites dynamic. For instance, the landing pages with the top products list is created using a PHP loop calling different custom fields from the different product pages - none of those products are coded individually.
+          <ul>
+            <li><a href="http://topmaleenhancements.com/" target="_blank">TopMaleEnhancements.com</a></li>
+            <li><a href="http://naturalmaleenhancement.org/" target="_blank">NaturalMaleEnhancement.org</a></li>
           </ul>
       </div> <!-- / .small-24 columns -->
     </div>
+    
     <div class="row">
       <div class="small-24 columns">
         <h2>Other</h2>
         <p>This site was coded and designed by me:<br>
         <a href="http://matthewjelalian.com/" target="_blank">MatthewJelalian.com</a></p>
+
+        <p>This page is set up using a <em>Wp_Query</em> and JavaScript/jQuery in showing and hiding the correct products:<br>
+        <a href="http://www.xpisupplements.com/products/" target="_blank">XPISupplements.com/products/</a></p>
       </div>
     </div>
   </div> <!-- / .portfolio -->
